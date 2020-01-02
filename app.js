04dfafa7659969f2143394bbfd1ef1353d1991bb
brunocasteluci <bruno.casteluci1996@gmail.com>
2020-01-02 18:03:42 -0300

    changed app.js for heroku

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
 // Server
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('The Yelpcamp server has started!!');
 });
