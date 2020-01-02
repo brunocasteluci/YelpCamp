df6a13913cdf5983ebff43248abf9fae59802d7c
brunocasteluci <bruno.casteluci1996@gmail.com>
2020-01-02 18:21:24 -0300

    changes

--- app.js
+++ app.js
@@ -17,11 +17,21 @@ const commentRoutes = require('./routes/comments'),
 
 app = express();
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
+mongoose
+  .connect(
+    'mongodb+srv://brunocasteluci:<password>@yelpcamp-xg3gm.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch(err => {
+    console.log('Error: ', err.messasge);
+  });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
